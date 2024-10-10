b25d36e59a5c70c071900c1b76132596ced299ba
Jefferson <jefffer233@gmail.com>
2024-10-10 15:37:35 -0300

    salvando proposta

---
+++ app/Livewire/Proposals/Create.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Livewire\Proposals;
+
+use Livewire\Component;
+use App\Models\Project;
+
+class Create extends Component
+{
+    public Project $project;
+
+    public bool $modal = true;
+
+    public string $email = '';
+    public int $hours = 0;
+    public bool $agree = false;
+
+    protected $rules = [
+        'email' => ['required', 'email'],
+        'hours' => ['required', 'numeric', 'min:1'],
+    ];
+
+    public function save() {
+        
+        $this->validate();
+        
+        if(!$this->agree) {
+            $this->addError('agree', 'Você precisa concordar com os termos de uso');
+            return;
+        }
+
+        $this->project->proposals()
+        ->updateOrCreate(
+            ['email' => $this->email],
+            ['hours' => $this->hours]
+        );
+
+        $this->modal = false;
+    }
+
+    public function render()
+    {
+        return view('livewire.proposals.create');
+    }
+}
