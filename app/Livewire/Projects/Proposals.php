7a88d9d3f772f94288f598aab4bbadcc746e6f0c
Jefferson <jefffer233@gmail.com>
2024-10-10 16:53:30 -0300

    avisando o componente proposals que uma nova proposta foi criada

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -4,11 +4,31 @@ namespace App\Livewire\Projects;
 
 use App\Models\Project;
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class Proposals extends Component
 {
     public Project $project;
 
+    public int $qty = 5;
+
+    public function proposals() {
+        return $this->project->proposals()
+            ->orderBy('hours')
+            ->paginate($this->qty);
+    }
+
+    public function lastProposalTime() {
+        return $this->project->proposals()
+            ->latest()->first()
+            ->created_at->diffForHumans();
+    }
+
+    public function loadMore() {
+        $this->qty += 5;
+    }
+
+    #[On('proposal::created')]
     public function render()
     {
         return view('livewire.projects.proposals');
